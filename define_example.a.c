1aba6aac47cc6d02371dd402f0771806d331d022
m7md-d <236364775+m7md-d@users.noreply.github.com>
2026-05-05 17:47:33 +0300

    fix some code and comment

--- define_example.a.c
+++ define_example.a.c
@@ -1,7 +1,9 @@
 #ifdef loop1
 for (int y = 1; y <= 10; y++)
 {
-    printf("y = %d\n", y);
+    #ifndef loop2
+    printf("y = %d\n", y); /* if loop1 is on and loop2 is off */
+    #endif
     #endif
     #ifdef loop2
     #include "define_example.b.c" /* if loop 2 is on this code will run */
